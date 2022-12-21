fce63455b17f69bae12cfd4b5d728ed53bd6105f
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-21 23:02:16 +0100

    Tests: Added test for Test-PSGalleryNameAvailability

---
+++ tests/Unit/Public/Test-PSGalleryNameAvailability.tests.ps1
@@ -0,0 +1,28 @@
+BeforeDiscovery {
+    $ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
+    $ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object {
+        ($_.Directory.Name -eq 'source') -and
+            $(try
+                {
+                    Test-ModuleManifest $_.FullName -ErrorAction Stop
+                }
+                catch
+                {
+                    $false
+                }) }
+    ).BaseName
+
+    Import-Module $ProjectName
+}
+
+InModuleScope $ProjectName {
+    Describe Test-PSGalleryNameAvailability {
+        Mock Invoke-GarbageCollect {} -Verifiable
+
+        Context 'default' {
+            It 'Should be true' {
+                $true | Should -BeTrue
+            }
+        }
+    }
+}
