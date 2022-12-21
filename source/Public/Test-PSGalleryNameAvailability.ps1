04218f670740885d082d7616cacfab0f2829be0e
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-21 22:56:56 +0100

    Changed module name to PSDev

---
+++ source/Public/Test-PSGalleryNameAvailability.ps1
@@ -0,0 +1,30 @@
+﻿function Test-PSGalleryNameAvailability
+{
+    <#
+        .DESCRIPTION
+        Retreive a list of ip and urls required for communication to and from Office 365.
+
+        .PARAMETER PackageName
+        Defines the package name to search for
+
+        .EXAMPLE
+        Test-PSGalleryNameAvailability -PackageName PowershellGet
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)]
+        [string]
+        $PackageName
+    )
+
+    $Response = Invoke-WebRequest -Uri "https://www.powershellgallery.com/packages/$PackageName" -SkipHttpErrorCheck
+    if ($Response.RawContent -like '*Page not found*')
+    {
+        return $true
+    }
+    else
+    {
+        return $false
+    }
+
+}
