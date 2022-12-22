f2c3f7fa3a5ce4b5469606f6fdad14ef966b08dd
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-22 13:03:42 +0100

    Test: Added Debug-String tests

---
+++ tests/Unit/Public/Debug-String.tests.ps1
@@ -0,0 +1,36 @@
+﻿BeforeDiscovery {
+    $ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
+    $ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object {
+            ($_.Directory.Name -eq 'source') -and
+            $(try
+                {
+                    Test-ModuleManifest $_.FullName -ErrorAction Stop
+                }
+                catch
+                {
+                    $false
+                })
+        }
+    ).BaseName
+
+    Import-Module $ProjectName -Force
+}
+
+InModuleScope $ProjectName {
+    Describe 'Debug-String' {
+        Context -Name 'Parameters' {
+            BeforeAll {
+                $Temp = "Te  st"
+            }
+            It 'String' {
+                Debug-String -InputObject $Temp | Should -BeExactly 'Te··st'
+            }
+            It 'CaretNotation' {
+                Debug-String -InputObject $Temp -CaretNotation | Should -BeExactly 'Te··st'
+            }
+            It 'ValueFromPipeline' {
+                $Temp | Debug-String | Should -BeExactly 'Te··st'
+            }
+        }
+    }
+}
