d6511018663265abbcd4f46728b77a19381abb40
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-22 13:03:32 +0100

    Test: Added Add-NumberFormater tests

---
+++ tests/Unit/Public/Add-NumberFormater.tests.ps1
@@ -0,0 +1,39 @@
+﻿BeforeDiscovery {
+    $ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
+    $ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object {
+            ($_.Directory.Name -eq 'source') -and
+            $(try
+                {
+                    Test-ModuleManifest $_.FullName -ErrorAction Stop
+                }
+                catch
+                {
+                    $false
+                })
+        }
+    ).BaseName
+
+    Import-Module $ProjectName -Force
+}
+
+InModuleScope $ProjectName {
+    Describe 'Add-NumberFormater' {
+        Context -Name 'Parameters' {
+            It -Name 'InputObject' {
+                $Result = Add-NumberFormater -InputObject 9472364
+                $Result.ToString() | Should -Be ('9{0}47 MN' -f (Get-Culture).NumberFormat.NumberDecimalSeparator)
+                [double]$Result | Should -Be 9472364
+            }
+            It -Name 'Type_DataSize' {
+                $Result = Add-NumberFormater -InputObject 9472364 -Type DataSize
+                $Result.ToString() | Should -Be ('9{0}03 MB' -f (Get-Culture).NumberFormat.NumberDecimalSeparator)
+                [double]$Result | Should -Be 9472364
+            }
+            It -Name 'ValueFromPipeline' {
+                $Result = 9472364 | Add-NumberFormater
+                $Result.ToString() | Should -Be ('9{0}47 MN' -f (Get-Culture).NumberFormat.NumberDecimalSeparator)
+                [double]$Result | Should -Be 9472364
+            }
+        }
+    }
+}
