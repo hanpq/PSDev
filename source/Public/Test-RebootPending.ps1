26263ca7afec861961f111916dbebf7566c94567
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-25 12:43:18 +0100

    Add: Test-RebootPending

---
+++ source/Public/Test-RebootPending.ps1
@@ -0,0 +1,22 @@
+﻿function Test-RebootPending
+{
+    <#
+    .DESCRIPTION
+        Queries the registry for the rebootpending key and returns the status
+    .PARAMETER Name
+        Description
+    .EXAMPLE
+        Test-RebootPending
+        Description of example
+    #>
+
+    [CmdletBinding()]
+    param(
+    )
+
+    PROCESS
+    {
+        $rebootRequired = Test-Path -Path 'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Component Based Servicing\RebootPending'
+        return $rebootRequired
+    }
+}
