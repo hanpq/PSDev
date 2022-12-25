31b60d78f2a21629aff45614b0df57fceabd8b80
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-25 12:42:27 +0100

    Add: Switch-Object

---
+++ source/Public/Switch-Object.ps1
@@ -0,0 +1,26 @@
+﻿function Switch-Object {
+    <#
+    .DESCRIPTION
+       Transposes an object, foreach parameter an object is created
+    .PARAMETER InputObject
+       Defined the object to transpose
+    .EXAMPLE
+       Get-Process | Select-object -first 1 | Switch-object
+       Description
+    #>
+    [CmdletBinding()]
+    param (
+        [Parameter(Mandatory, ValueFromPipeline)]$InputObject
+    )
+    PROCESS {
+        $InputObject | ForEach-Object {
+            $instance = $_
+            $instance.PSObject.Properties.Name | ForEach-Object {
+                [PSCustomObject]@{
+                    Name  = $_
+                    Value = $instance.$_
+                }
+            }
+        }
+    }
+}
