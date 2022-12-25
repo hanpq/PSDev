fda37bd12ca294d762f816b13a761e6467fa224b
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-25 13:13:23 +0100

    Fix: Exclude user environment scope tests on UNIX bases OS

--- tests/Unit/Public/Set-EnvironmentVariable.tests.ps1
+++ tests/Unit/Public/Set-EnvironmentVariable.tests.ps1
@@ -18,22 +18,23 @@ BeforeDiscovery {
 
 InModuleScope $ProjectName {
     Describe 'Set-EnvironmentVariable' {
-        Context -Name 'When setting EnvironmentVariable' {
+        Context -Name 'When setting Process EnvironmentVariable' -Fixture {
             BeforeAll {
-                #Set-EnvironmentVariable -Name 'Test1' -Value 'Test1' -Target Machine
                 Set-EnvironmentVariable -Name 'Test2' -Value 'Test2' -Target Process
+            }
+            AfterAll {
+                Set-EnvironmentVariable -Name 'Test2' -Value '' -Target Process
+            }
+            It -Name 'Should exist a environmentvariable in process scope' -Test {
+                [System.Environment]::GetEnvironmentVariable('Test2', 'Process') | Should -Not -BeNullOrEmpty
+            }
+        }
+        Context -Name 'When setting User EnvironmentVariable' -Skip:($IsLinux -or $IsMacOS) -Fixture {
+            BeforeAll {
                 Set-EnvironmentVariable -Name 'Test3' -Value 'Test3' -Target User
             }
             AfterAll {
-                #Set-EnvironmentVariable -Name 'Test1' -Value '' -Target Machine
-                Set-EnvironmentVariable -Name 'Test2' -Value '' -Target Process
                 Set-EnvironmentVariable -Name 'Test3' -Value '' -Target User
-            }
-            #It -Name 'Should exist a environmentvariable in machine scope' -Test {
-            #    [System.Environment]::GetEnvironmentVariable('Test1', 'Machine') | should -not -BeNullOrEmpty
-            #}
-            It -Name 'Should exist a environmentvariable in process scope' -Test {
-                [System.Environment]::GetEnvironmentVariable('Test2', 'Process') | Should -Not -BeNullOrEmpty
             }
             It -Name 'Should exist a environmentvariable in user scope' -Test {
                 [System.Environment]::GetEnvironmentVariable('Test3', 'User') | Should -Not -BeNullOrEmpty
