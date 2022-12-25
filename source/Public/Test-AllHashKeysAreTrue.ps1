7dc2ee5dd2d139ed17dd2c72507ff5392b580e47
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-25 12:42:56 +0100

    Add: Test-AllHashKeysAreTrue

---
+++ source/Public/Test-AllHashKeysAreTrue.ps1
@@ -0,0 +1,27 @@
+﻿function Test-AllHashKeysAreTrue {
+    <#
+    .DESCRIPTION
+        This functions checks that all values of a hashtable evaluates to true. For values not of type boolean, a typecast to bool is performed.
+    .PARAMETER HashTable
+        Defines the hashtable object to test
+    .EXAMPLE
+        Validate-AllHashKeysAreTrue
+        Description of example
+    #>
+
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory, ValueFromPipeline)][hashtable]$HashTable
+    )
+
+    PROCESS {
+        $AllTrue = $true
+        foreach ($Key in $HashTable.Keys) {
+            if ($HashTable.$Key -as [boolean] -eq $false) {
+                $AllTrue = $false
+                break
+            }
+        }
+        Write-Output $AllTrue
+    }
+}
