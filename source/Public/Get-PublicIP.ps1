d860fa95daf4e30153fac6570c2a9455deb0cdd4
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-25 12:41:09 +0100

    Add: Get-PublicIP

---
+++ source/Public/Get-PublicIP.ps1
@@ -0,0 +1,18 @@
+﻿function Get-PublicIP {
+    <#
+    .DESCRIPTION
+        Get the current public facing IP address
+    .PARAMETER Name
+        Description
+    .EXAMPLE
+        Get-PublicIP
+        Description of example
+    #>
+
+    [CmdletBinding()]
+    param(
+    )
+    PROCESS {
+        Invoke-RestMethod -Uri 'http://ipinfo.io/json'
+    }
+}
