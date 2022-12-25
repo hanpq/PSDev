5ca015b50ccb0b8977eeb7fb7704d331140e0610
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-25 12:40:48 +0100

    Add: Get-Color

---
+++ source/Public/Get-Color.ps1
@@ -0,0 +1,19 @@
+﻿function Get-Color
+{
+    <#
+    .DESCRIPTION
+        Functions showing all consolecolors
+    .EXAMPLE
+        Get-Color
+        Shows all the available consolecolors
+    #>
+    [Diagnostics.CodeAnalysis.SuppressMessageAttribute('PSAvoidUsingWriteHost', '', Justification = 'Interactive command')]
+    [CmdletBinding()]
+    param()
+
+    [enum]::GetNames([consolecolor]) | ForEach-Object {
+        Write-Host (' {0}' -f $_).PadRight(15) -BackgroundColor $_ -NoNewline
+        Write-Host ' ' -NoNewline
+        Write-Host ('{0} ' -f $_).PadLeft(15) -BackgroundColor $_ -ForegroundColor Black
+    }
+}
