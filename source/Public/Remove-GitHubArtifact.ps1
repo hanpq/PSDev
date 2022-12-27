3c1c6a861f2663b74aab19ceca8cdcc0464f10e9
Hannes Palmquist <hannes.palmquist@outlook.com>
2022-12-27 20:40:54 +0100

    Add: Function Remove-GitHubArtifacts

---
+++ source/Public/Remove-GitHubArtifact.ps1
@@ -0,0 +1,115 @@
+﻿function Remove-GitHubArtifact
+{
+    <#
+    .SYNOPSIS
+        Cleanup artifacts from GitHub repo
+    .DESCRIPTION
+        This script will remove all artifacts for a single repos or all repos for a given user
+    .PARAMETER GitHubSecret
+        Defines the GitHubSecret (API Key) to use
+    .PARAMETER GitHubOrg
+        Defines the GitHub owner user name
+    .PARAMETER Repo
+        Optionally specify a repo to only remove artifacts for that specific repo
+    .PARAMETER PageSize
+        Optionally specify the PageSize when retreiving repos and artifacts. Valid values are in range of 1..100. Default is 30.
+    .LINK
+        https://getps.dev/blog/cleanup-github-artifacts
+    .EXAMPLE
+        Remove-GitHubArtifact -GitHubSecret "ABC" -GitHubOrg "user"
+
+        Running this function without specifying a repo will cleanup all artifacts for all repos
+    .EXAMPLE
+        Remove-GitHubArtifact -GitHubSecret "ABC" -GitHubOrg "user" -Repo "RepoName"
+
+        Running the script with a specified repo will cleanup all artifacts for that repo
+#>
+    [CmdletBinding(SupportsShouldProcess)]
+    param(
+        [parameter(Mandatory)]
+        [string]
+        $GitHubSecret,
+
+        [parameter(Mandatory)]
+        [string]
+        $GitHubOrg,
+
+        [parameter()]
+        [string]
+        $RepoName,
+
+        [parameter()]
+        [ValidateRange(1, 100)]
+        [int]
+        $PageSize = 30
+    )
+
+    $PSDefaultParameterValues = @{
+        'Invoke-RestMethod:Headers' = @{Accept = 'application/vnd.github+json'; Authorization = "Bearer $GitHubSecret" }
+    }
+
+    # Find repos
+    if ($RepoName)
+    {
+        $Repos = Invoke-RestMethod -Method get -Uri "https://api.github.com/repos/$GitHubOrg/$RepoName"
+    }
+    else
+    {
+        $Repos = [System.Collections.Generic.List[Object]]::New()
+        $PageID = 1
+        do
+        {
+            $Result = Invoke-RestMethod -Method get -Uri "https://api.github.com/user/repos?per_page=$PageSize&page=$PageID"
+            if ($Result)
+            {
+                $Repos.AddRange([array]$Result)
+            }
+            $PageID++
+        } until ($Result.Count -lt $PageSize)
+    }
+
+    foreach ($repo in $repos)
+    {
+        Write-Verbose -Message "Processing repo $($repo.name)"
+
+        # Define result object
+        $ObjectHash = [ordered]@{
+            Repo              = $Repo.Name
+            Artifacts_Found   = 0
+            Artifacts_Removed = 0
+            Artifacts_SizeMB  = 0
+            Artifacts         = [System.Collections.Generic.List[Object]]::New()
+        }
+
+        # Find artifacts
+        $Artifacts = [System.Collections.Generic.List[Object]]::New()
+        $PageID = 1
+        do
+        {
+            $Result = Invoke-RestMethod -Method get -Uri "https://api.github.com/repos/$GitHubOrg/$($Repo.Name)/actions/artifacts?per_page=$PageSize&page=$PageID" | Select-Object -ExpandProperty artifacts
+            if ($Result)
+            {
+                $Artifacts.AddRange([array]$Result)
+            }
+            $PageID++
+        } until ($Result.Count -lt $PageSize)
+
+        # Remove artifacts
+        if ($artifacts)
+        {
+            $ObjectHash.Artifacts_Found = $Artifacts.Count
+            $ObjectHash.Artifacts_SizeMB = (($Artifacts | Measure-Object -Sum -Property size_in_bytes).Sum / 1MB)
+            foreach ($artifact in $artifacts)
+            {
+                if ($PSCmdlet.ShouldProcess("Artifact: $($artifact.name) in Repo: $($Repo.Name)", 'DELETE'))
+                {
+                    $Result = Invoke-RestMethod -Method DELETE -Uri "https://api.github.com/repos/$GitHubOrg/$($Repo.Name)/actions/artifacts/$($artifact.id)"
+                    $ObjectHash.Artifact_Removed++
+                }
+            }
+        }
+
+        # Return resultobject
+        [pscustomobject]$ObjectHash
+    }
+}
---
+++ tests/Unit/Public/Remove-GitHubArtifact.tests.ps1
@@ -0,0 +1,62 @@
+BeforeDiscovery {
+    $ProjectPath = "$PSScriptRoot\..\..\.." | Convert-Path
+    $ProjectName = (Get-ChildItem $ProjectPath\*\*.psd1 | Where-Object {
+            ($_.Directory.Name -eq 'source') -and
+            $(try
+                {
+                    Test-ModuleManifest $_.FullName -ErrorAction Stop
+                }
+                catch
+                {
+                    $false
+                })
+        }
+    ).BaseName
+
+    Import-Module $ProjectName -Force
+}
+
+InModuleScope $ProjectName {
+    Describe 'Remove-GitHubArtifact' {
+        Context 'Without specified repo, no artifacts found' {
+            BeforeAll {
+                Mock -CommandName Invoke-RestMethod -MockWith {
+                    return ('[{"name":"PSScriptInfo"},{"name":"PSSort"}]' | ConvertFrom-Json)
+                } -ParameterFilter { $uri -match '.*repos\?.*' }
+                Mock -CommandName Invoke-RestMethod -MockWith {
+                    return $null
+                } -ParameterFilter { $uri -match '.*artifacts\?.*' }
+                Mock -CommandName Invoke-RestMethod -MockWith {
+                    return $null
+                } -ParameterFilter { $uri -match '.*artifacts\/.*' -and $method -eq 'DELETE' } -Verifiable
+            }
+            It 'Should not throw' {
+                { Remove-GitHubArtifact -GitHubSecret $env:GitHubToken -GitHubOrg hanpq } | Should -Not -Throw
+            }
+            It 'Should not call delete artifact' {
+                Remove-GitHubArtifact -GitHubSecret $env:GitHubToken -GitHubOrg hanpq
+                Should -Invoke -CommandName Invoke-RestMethod -ParameterFilter { $uri -match '.*artifacts\/.*' -and $method -eq 'DELETE' } -Times 0
+            }
+        }
+        Context 'With specified repo, no artifacts found' {
+            BeforeAll {
+                Mock -CommandName Invoke-RestMethod -MockWith {
+                    return ('[{"name":"PSScriptInfo"}]' | ConvertFrom-Json)
+                } -ParameterFilter { $uri -match '.*repos\/.*' }
+                Mock -CommandName Invoke-RestMethod -MockWith {
+                    return $null
+                } -ParameterFilter { $uri -match '.*artifacts\?.*' }
+                Mock -CommandName Invoke-RestMethod -MockWith {
+                    return $null
+                } -ParameterFilter { $uri -match '.*artifacts\/.*' -and $method -eq 'DELETE' } -Verifiable
+            }
+            It 'Should not throw' {
+                { Remove-GitHubArtifact -Repo 'PSScriptInfo' -GitHubSecret $env:GitHubToken -GitHubOrg hanpq } | Should -Not -Throw
+            }
+            It 'Should not call delete artifact' {
+                Remove-GitHubArtifact -Repo 'PSScriptInfo' -GitHubSecret $env:GitHubToken -GitHubOrg hanpq
+                Should -Invoke -CommandName Invoke-RestMethod -ParameterFilter { $uri -match '.*artifacts\/.*' -and $method -eq 'DELETE' } -Times 0
+            }
+        }
+    }
+}
