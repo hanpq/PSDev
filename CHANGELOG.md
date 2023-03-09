c8bcf3cd4256c68e90656ccf5f72cd1c81936bd3
Hannes Palmquist <hannes.palmquist@outlook.com>
2023-03-09 13:30:23 +0100

    Added script Get-Selector

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 ### Addded
 
 - New function: Convert-Metric
+- New function: Get-Selector
 
 ## [1.3.0] - 2023-01-12
 
---
+++ source/Public/Get-Selector.ps1
@@ -0,0 +1,76 @@
+﻿function Get-Selector
+{
+    <#
+    .DESCRIPTION
+        Increments a string of characters
+    .PARAMETER PreviousSelector
+        Defines the string to be incremented
+    .EXAMPLE
+        Get-Selector -PreviousSelector AA
+
+        Would return AB
+    .EXAMPLE
+        Get-Selector -PreviousSelector ZZ
+
+        Would return AAA
+    #>
+
+    param (
+        [string]$PreviousSelector
+    )
+
+    $Length = $PreviousSelector.Length
+
+    $SelectorString = $PreviousSelector.PadLeft($Length, '0')
+    $SelectorCharArray = $SelectorString.ToCharArray()
+
+    function Increment
+    {
+        param(
+            [char[]]$CharArray,
+            [int]$position
+        )
+
+        if ($position -lt 0)
+        {
+            throw 'Selector wrapped around, exceeding string length capability. Increase string length to accomodate more values.'
+        }
+
+        if ($CharArray[$position] -eq 90)
+        {
+            $CharArray[$position] = 65
+            $CharArray = Increment -CharArray $CharArray -position ($position - 1)
+        }
+        else
+        {
+            if ($CharArray[$position] -eq '0')
+            {
+                $CharArray[$position] = 65
+            }
+            else
+            {
+                $CharArray[$position] = [char](($CharArray[$position] -as [int]) + 1)
+            }
+        }
+        return $CharArray
+    }
+
+    try
+    {
+        $Result = Increment -CharArray $SelectorCharArray -position ($Length - 1)
+    }
+    catch
+    {
+        if ($_.Exception.message -like 'Selector wrapped around*')
+        {
+            $SelectorString = $PreviousSelector.PadLeft($Length + 1, '0')
+            $SelectorCharArray = $SelectorString.ToCharArray()
+            $Result = Increment -CharArray $SelectorCharArray -position ($Length)
+        }
+        else
+        {
+            throw $_
+        }
+    }
+    return [string](($Result -join '').Trim('0'))
+}
