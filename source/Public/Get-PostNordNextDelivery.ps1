036e6bf8dc853f06d006d7b41795d3b45904624e
hanpq <hannes.palmquist@outlook.com>
2025-10-10 08:10:15 +0200

    Added Get-PostNordNextDelivery

---
+++ source/Public/Get-PostNordNextDelivery.ps1
@@ -0,0 +1,24 @@
+﻿function Get-PostNordNextDelivery
+{
+    <#
+    .DESCRIPTION
+        Get the next delivery date from PostNord
+    .PARAMETER Postalcode
+        Defines the postal code to get the next delivery date for
+    .EXAMPLE
+        Get-PostNordNextDelivery -PostalCode 12345
+
+        postalCode city      delivery         upcoming
+        ---------- ----      --------         --------
+        12345      Stockholm 27 januari, 2025 29 januari, 2025
+    #>
+
+    [CmdletBinding()]
+    param(
+        [int]$PostalCode
+    )
+    PROCESS
+    {
+        Invoke-RestMethod -Uri "https://portal.postnord.com/api/sendoutarrival/closest?postalCode=$PostalCode"
+    }
+}
